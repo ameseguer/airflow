d1e78ee634268b8855ab4ca02765b04e0cfd72f8
Andres Meseguer Rojas <amesegue@akamai.com>
2021-05-25 11:25:36 -0600

    EAA 3 attempts

--- dags/eaatest.py
+++ dags/eaatest.py
@@ -24,7 +24,7 @@ default_args = {
     'email': ['amesegue@akamai.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 0,
+    'retries': 3,
     'retry_delay': timedelta(minutes=5),
 }
 
